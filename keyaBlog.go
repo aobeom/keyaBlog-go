f85bcd67c599d92d646a5ab73236037733d1c87e
csm <csm@eptonic.com>
2018-06-01 11:34:25 +0800

    optimization

--- keyaBlog.go
+++ keyaBlog.go
@@ -10,13 +10,27 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
-	"strings"
 	"runtime"
+	"strings"
+	"sync"
+
 	"github.com/PuerkitoBio/goquery"
 )
 
-var imgURL chan string
-var quit chan bool
+// DLserver 下载控制器
+type DLserver struct {
+	WG    sync.WaitGroup
+	Gonum chan string
+}
+
+// Blogs 博客结构
+type Blogs struct {
+	title  string
+	author string
+	date   string
+	text   string
+	imgs   []string
+}
 
 // getCurrentDirectory 获取当前路径
 func getCurrentDirectory() string {
@@ -67,10 +81,13 @@ func ImgURLGet(blogText string) (imgs []string) {
 	return
 }
 
-// SaveToText 合并换行符
-func SaveToText(blogRaw *goquery.Document) {
-	blogMain, imgs, folder, savename := FormatInfo(blogRaw)
-	mainFolder := getCurrentDirectory() + "//" + folder
+// SaveToText 保存内容
+func SaveToText(blogRaw *goquery.Document, dl *DLserver, thread int) {
+	blogInfo, savename := FormatInfo(blogRaw)
+	// 创建保存的目录 文件名
+	blogMain := blogInfo.title + "\r\n" + blogInfo.date + "\r\n" + blogInfo.text
+	imgs := blogInfo.imgs
+	mainFolder := getCurrentDirectory() + "//" + blogInfo.author
 	mainExist, _ := pathExists(mainFolder)
 	if !mainExist {
 		os.Mkdir(mainFolder, os.ModePerm)
@@ -81,73 +98,75 @@ func SaveToText(blogRaw *goquery.Document) {
 		os.Mkdir(subFolder, os.ModePerm)
 	}
 	filename := subFolder + "//" + savename + ".txt"
-	runtime.GOMAXPROCS(4)
-	imgURL = make(chan string)
-	quit = make(chan bool)
-	go downloadEngine(imgURL, quit, subFolder)
-	for _, i := range imgs {
-		imgURL <- i
+
+	// 启用多核
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	// 用channel阻塞控制goroutine的数量
+	dl.Gonum = make(chan string, thread)
+	// 当前任务数量
+	tasks := len(imgs)
+	dl.WG.Add(tasks)
+	log.Printf("Total [%d]\n", tasks)
+	// 执行下载
+	for _, img := range imgs {
+		dl.Gonum <- img
+		go downloadEngine(img, subFolder, dl)
 	}
-	quit <- false
+	dl.WG.Wait()
 	ioutil.WriteFile(filename, []byte(blogMain), 0644)
+	log.Printf("All is down")
 }
 
-// downloadEngine
-func downloadEngine(imgs chan string, quit chan bool, path string) {
-	for {
-		select {
-		case img := <-imgs:
-			client := &http.Client{}
-			req, _ := http.NewRequest("GET", img, nil)
-			req.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
-			res, _ := client.Do(req)
-			defer res.Body.Close()
-			urlCut := strings.Split(img, "/")
-			filename := urlCut[len(urlCut)-1]
-			savePath := path + "//" + filename
-			file, _ := os.Create(savePath)
-			io.Copy(file, res.Body)
-		case <-quit:
-			break
-		}
-	}
+// downloadEngine 下载函数
+func downloadEngine(img string, path string, dl *DLserver) {
+	client := &http.Client{}
+	urlCut := strings.Split(img, "/")
+	filename := urlCut[len(urlCut)-1]
+	req, _ := http.NewRequest("GET", img, nil)
+	log.Printf("[%s is Start..]\n", filename)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
+	res, _ := client.Do(req)
+	defer res.Body.Close()
+	savePath := path + "//" + filename
+	file, _ := os.Create(savePath)
+	io.Copy(file, res.Body)
+	log.Printf("---- %s is done\n", filename)
+	dl.WG.Done()
+	// 每个goroutine完成后取出
+	<-dl.Gonum
 }
 
 // FormatInfo 格式化输出内容
-func FormatInfo(blogRaw *goquery.Document) (Blog string, imgs []string, folder string, savename string) {
+func FormatInfo(blogRaw *goquery.Document) (BlogInfo Blogs, Date string) {
 	doc := blogRaw
 	Newbody := doc.Find("article")
-	BlogTitle := Newbody.Find(".box-ttl h3").Text()
-	// fmt.Print(RemoveBlank(BlogTitle))
-	Blog = Blog + RemoveBlank(BlogTitle) + "\r\n"
-	BlogAuthor := Newbody.Find(".name").Text()
-	// fmt.Println(RemoveBlank(BlogAuthor))
-	// Blog = Blog + RemoveBlank(BlogAuthor) + "\r\n"
-	BlogDate := Newbody.Find(".box-bottom li").Text()
-	// fmt.Println(RemoveBlank(BlogDate))
-	BlogDateFormat := strings.Replace(RemoveBlank(BlogDate), "/", "-", -1)
-	BlogDateFormat = strings.Replace(RemoveBlank(BlogDateFormat), " ", "-", -1)
-	BlogDateFormat = strings.Replace(RemoveBlank(BlogDateFormat), ":", "-", -1)
-	Blog = Blog + RemoveBlank(BlogDate) + "\r\n"
-	// fmt.Println(Blog)
 	BlogCont, _ := Newbody.Find(".box-article").Html()
-	BlogCont = PurifyBlog(BlogCont)
-	// fmt.Println(BlogCont)
-	Blog = Blog + MergeLine(BlogCont)
-	imgs = ImgURLGet(Blog)
-	folder = RemoveBlank(BlogAuthor)
-	savename = BlogDateFormat
+
+	BlogInfo.title = RemoveBlank(Newbody.Find(".box-ttl h3").Text())
+	BlogInfo.author = RemoveBlank(Newbody.Find(".name").Text())
+	BlogInfo.date = RemoveBlank(Newbody.Find(".box-bottom li").Text())
+
+	PurifyText := PurifyBlog(BlogCont)
+	BlogInfo.text = MergeLine(PurifyText)
+	BlogInfo.imgs = ImgURLGet(PurifyText)
+
+	// 格式化时间作为目录名
+	BlogDateFormat := strings.Replace(RemoveBlank(BlogInfo.date), "/", "-", -1)
+	BlogDateFormat = strings.Replace(RemoveBlank(BlogDateFormat), " ", "-", -1)
+	Date = strings.Replace(RemoveBlank(BlogDateFormat), ":", "-", -1)
 	return
 }
 
 // GetBlogRaw 获取原始内容
 func GetBlogRaw(url string) *goquery.Document {
-	fmt.Println("Get Blog Info...")
+	log.Println("Get Blog Info...")
+	// 自定义UA的Get请求
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
 	res, _ := client.Do(req)
 	defer res.Body.Close()
+	// 返回goquery的结构
 	body, _ := goquery.NewDocumentFromReader(res.Body)
 	return body
 }
@@ -158,7 +177,7 @@ func main() {
 	fmt.Print("Url: ")
 	data, _, _ := reader.ReadLine()
 	url := string(data)
+	dl := new(DLserver)
 	blogRaw := GetBlogRaw(url)
-	SaveToText(blogRaw)
+	SaveToText(blogRaw, dl, 4)
 }
-
